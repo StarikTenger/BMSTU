47ffc9a77a537552814c93bdf365ac23a48de33d
StarikTenger <stariktenger@gmail.com>
2020-09-26 16:14:47 +0300

    c: tasks 9-10

---
+++ c/task10.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+unsigned char array[] = {
+	78,
+	91,
+	37,
+	107,
+	111,
+	192,
+	228,
+	199,
+	128,
+	201,
+	67,
+	210,
+	182,
+	10,
+	6,
+	246,
+	24,
+	46,
+	234,
+	91,
+	4,
+	22,
+	177,
+	139,
+	142,
+	29,
+	148,
+	9,
+	3,
+	128,
+	231,
+	131,
+	90,
+	107,
+	168,
+	241,
+	251,
+	59,
+	196,
+	108,
+	59,
+	91,
+	36,
+	135,
+	251,
+	184,
+	179,
+	221
+};
+
+int compare(void *a, void *b)
+{
+	return (int)(*(unsigned char*)a) - (int)(*(unsigned char*)b);
+}
+
+int maxarray(void*, unsigned long, unsigned long,
+	int (*)(void *a, void *b));
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", maxarray(array, 48, sizeof(unsigned char), compare));
+	return 0;
+}
+
+int maxarray(void  *base, unsigned long nel, unsigned long width, 
+    int (*compare)(void *a, void *b)) 
+{
+    void* max = base;
+    int idx = 0;
+    for (unsigned long i = 0; i < nel; i++) 
+    {
+        void* elem = (void*)((char*)base + width * i);
+        if (compare(elem, max) > 0) {
+            max = elem;
+            idx = i;
+        }
+    }
+    return idx;
+}
--- c/task9.c
+++ c/task9.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int array[] = {
-        1000000000,
-        2000000000,
-        3000000000,
-        4000000000,
-        5000000000
-};
+#include <string.h>
 
 void revarray(void *base, unsigned long nel, unsigned long width) {
-    for (unsigned long i = 0; i  < nel; i++) {
-        
+    for (unsigned long i = 0; i * 2 < nel; i++) {
+        unsigned long j = nel - i - 1;
+        void* buff = malloc(width);
+        memmove(buff, ((char*)base + i * width), width);
+        memmove(((char*)base + i * width), ((char*)base + j * width), width);
+        memmove(((char*)base + j * width), buff, width);
+        free(buff);
     }
 }
 
-int main(int argc, char **argv) {
+
+int array[] = {
+        1, 2, 3, 4, 5
+};
+
+int main(int argc, char **argv)
+{
         revarray(array, 5, sizeof(int));
 
         int i;
