edaa14189f79b18a64a48e0d36c9e28a117de854
StarikTenger <stariktenger@gmail.com>
2020-12-01 18:51:20 +0300

    scheme lab 3 - serialize

--- scheme-labs/LAB_4.scm
+++ scheme-labs/LAB_4.scm
@@ -18,7 +18,27 @@
 
 (map 1/x '(1 2 3 4 5)) ; ВЕРНЕТ список значений в программу
 
-(map 1/x '(-2 -1 0 1 2)) ; ВЫВЕДЕТ в консоль сообщение и завершит работу программы
+;(map 1/x '(-2 -1 0 1 2)) ; ВЫВЕДЕТ в консоль сообщение и завершит работу программы
 
 
-;; === 2 : MEMORIZATION ================================================
+;; === 2 : SERIALIZATION ===============================================
+
+(define (save-data data path)
+  (call-with-output-file path
+    (lambda (output-port)
+      (display data output-port))))
+
+(define (load-data path)
+  (let ((input (open-input-file path)))
+    (read input)))
+
+(define (calculate-strings-from-input-port input-port symbol-prev)
+  (let ((symbol (read-char input-port)))
+    (begin
+      (+
+       (if (and (equal? symbol-prev #\linefeed) (not (equal? symbol #\return)) (not (eof-object? symbol))) 1 0)
+       (if (eof-object? symbol) 0 (calculate-strings-from-input-port input-port symbol))
+       ))))
+
+(define (count-strings path)
+  (calculate-strings-from-input-port (open-input-file path) #\newline))
