d9da2c2756f4b142d1d74cc0b8c9a09f9e35197a
StarikTenger <StarikTenger@gmail.com>
2021-04-27 10:56:50 +0300

    java: lab6: final fixes

--- java/gui/src/PictureForm.java
+++ java/gui/src/PictureForm.java
@@ -11,6 +11,9 @@ public class PictureForm {
     private JSlider scale;
     private JSlider offsetY;
     private JSlider offsetX;
+    private JSlider scaleY;
+    private JLabel stepText;
+    private JCheckBox stripes;
 
     public PictureForm() {
         scale.addChangeListener(new ChangeListener() {
@@ -18,9 +21,15 @@ public class PictureForm {
                 canvas.setScale(scale.getValue());
             }
         });
+        scaleY.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                canvas.setScaleY(scaleY.getValue());
+            }
+        });
         step.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 canvas.setStep(step.getValue());
+                stepText.setText("Step = " +  String.format("%.2f", step.getValue()/100.));
             }
         });
         offsetX.addChangeListener(new ChangeListener() {
@@ -33,6 +42,28 @@ public class PictureForm {
                 canvas.setOffsetY(offsetY.getValue());
             }
         });
+        offsetY.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                canvas.setOffsetY(offsetY.getValue());
+            }
+        });
+        stripes.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                canvas.setStriped(stripes.isSelected());
+            }
+        });
+
+        step.setValue(50);
+        scale.setValue(100);
+        scaleY.setValue(10);
+        offsetX.setValue(0);
+        offsetY.setValue(0);
+        canvas.setScale(scale.getValue());
+        canvas.setScaleY(scaleY.getValue());
+        canvas.setStep(step.getValue());
+        stepText.setText("Step = " +  String.format("%.2f", step.getValue()/100.));
+        canvas.setOffsetX(offsetX.getValue());
+        canvas.setOffsetY(offsetY.getValue());
     }
 
     public static void main(String[] args) {
@@ -49,40 +80,41 @@ public class PictureForm {
 }
 
 class Canvas extends JPanel {
-    private double a = 1;
+    private double scaleY = 1;
     private double b = 1;
     private double scale = 10;
     private double offsetX = 0;
     private double offsetY = 0;
     private double step = 10;
+    private boolean isStriped = false;
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        for (int i = 0; i < 600; i++) {
-            g.setColor(Color.YELLOW);
-            if (i % 2 == 0) {
-                g.setColor(Color.BLUE);
+        for (int i = -600; i < 600; i++) {
+            g.setColor(Color.BLACK);
+            if (i % 2 == 0 && isStriped) {
+                g.setColor(Color.DARK_GRAY);
             }
             double xs[] = {i * step, i * step + step, i * step + step, i * step};
-            int xPoly[] = Arrays.stream(xs).map(x->(x-offsetX)*scale).mapToInt(x -> (int)x).toArray();
+            int xPoly[] = Arrays.stream(xs).map(x->(x-offsetX)*scale + 300).mapToInt(x -> (int)x).toArray();
             double ys[] = {0, 0, f(i * step + step), f(i * step)};
-            int yPoly[] = Arrays.stream(ys).map(x->-(x-offsetY)*scale).mapToInt(x -> (int)x).toArray();
+            int yPoly[] = Arrays.stream(ys).map(x->-(x-offsetY)*scale*scaleY + 300).mapToInt(x -> (int)x).toArray();
             Polygon poly = new Polygon(xPoly, yPoly, xPoly.length);
             g.fillPolygon(poly);
         }
     }
 
     private double f(double x) {
-        return Math.cos(a * x) * b;
+        return x * Math.cos(x);
     }
 
     public void setRadius(int radius) {
         repaint();
     }
 
-    public void setA(double a) {
-        this.a = a;
+    public void setScaleY(double scaleY) {
+        this.scaleY = scaleY / 10;
         repaint();
     }
 
@@ -108,6 +140,11 @@ class Canvas extends JPanel {
 
     public void setStep(double step) {
         this.step = step / 100;
+        repaint();
+    }
+
+    public void setStriped(boolean striped) {
+        isStriped = striped;
         repaint();
     }
 }
